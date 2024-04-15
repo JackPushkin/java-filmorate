2102d8aa3466c11a235b99ea840c9356c946c337
Jack <evgepuschckin@yandex.ru>
2024-04-15 17:43:01 +0300

    Correct makeFilm method and getFilms query

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmDbStorage.java
@@ -3,7 +3,10 @@ package ru.yandex.practicum.filmorate.storage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.exeption.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
@@ -13,6 +16,7 @@ import ru.yandex.practicum.filmorate.storage.interfaces.FilmStorage;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -24,6 +28,7 @@ public class FilmDbStorage implements FilmStorage {
 
     private final JdbcTemplate jdbcTemplate;
     private final SimpleJdbcInsert simpleJdbcInsert;
+    private final NamedParameterJdbcTemplate parameterJdbcTemplate;
 
     @Autowired
     public FilmDbStorage(JdbcTemplate jdbcTemplate) {
@@ -31,6 +36,7 @@ public class FilmDbStorage implements FilmStorage {
         simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
                 .withTableName("films")
                 .usingGeneratedKeyColumns("id_film");
+        parameterJdbcTemplate = new NamedParameterJdbcTemplate(jdbcTemplate);
     }
 
     @Override
@@ -64,13 +70,15 @@ public class FilmDbStorage implements FilmStorage {
     @Override
     public List<Film> getFilms() {
         String sqlQuery = "SELECT * FROM films f JOIN mpa_rating mr ON f.id_rating = mr.id_rating";
-        return jdbcTemplate.query(sqlQuery, (rs, numRow) -> makeFilm(rs));
+        Map<Integer, Set<Genre>> genres = getFilmsGenresMap(null);
+        return jdbcTemplate.query(sqlQuery, (rs, numRow) -> makeFilm(rs, genres));
     }
 
     @Override
     public Film getFilmById(Integer filmId) {
+        Map<Integer, Set<Genre>> genres = getFilmsGenresMap(Set.of(filmId));
         String sqlQuery = "SELECT * FROM films f JOIN mpa_rating mr ON f.id_rating = mr.id_rating WHERE id_film = ?";
-        List<Film> films = jdbcTemplate.query(sqlQuery, (rs, rowNum) -> makeFilm(rs), filmId);
+        List<Film> films = jdbcTemplate.query(sqlQuery, (rs, rowNum) -> makeFilm(rs, genres), filmId);
         if (films.isEmpty()) {
             throw new NotFoundException(String.format("Film with id=%d not found", filmId));
         }
@@ -79,6 +87,7 @@ public class FilmDbStorage implements FilmStorage {
 
     @Override
     public List<Film> getPopularFilms(Integer count) {
+        Map<Integer, Set<Genre>> genres = getFilmsGenresMap(null);
         String sqlQuery = "SELECT f.id_film, f.title, f.description, f.release_date, f.duration, f.id_rating, mr.rating_name " +
                           "FROM films f " +
                           "JOIN mpa_rating mr ON f.id_rating = mr.id_rating " +
@@ -86,7 +95,7 @@ public class FilmDbStorage implements FilmStorage {
                           "GROUP BY f.id_film " +
                           "ORDER BY COUNT(l.id_user) DESC " +
                           "LIMIT ?";
-        return jdbcTemplate.query(sqlQuery, (rs, numRow) -> makeFilm(rs), count);
+        return jdbcTemplate.query(sqlQuery, (rs, numRow) -> makeFilm(rs, genres), count);
     }
 
     @Override
@@ -124,15 +133,32 @@ public class FilmDbStorage implements FilmStorage {
         jdbcTemplate.update(sqlQuery, film.getId());
     }
 
-    private Set<Genre> getFilmGenresSet(Integer filmId) {
-        String sqlQuery = "SELECT g.id_genre, g.genre_name " +
-                          "FROM films_genres fg " +
-                          "JOIN genres g ON fg.id_genre = g.id_genre " +
-                          "WHERE fg.id_film = ?";
-        return new HashSet<>(jdbcTemplate.query(sqlQuery, ((rs, rowNum) -> Genre.builder()
-                .id(rs.getInt("id_genre"))
-                .name(rs.getString("genre_name"))
-                .build()), filmId));
+    private Map<Integer, Set<Genre>> getFilmsGenresMap(Set<Integer> ids) {
+        String sqlQuery;
+        SqlRowSet rowSet;
+        if (ids == null) {
+            sqlQuery = "SELECT * FROM films_genres fg JOIN genres g ON fg.id_genre = g.id_genre";
+            rowSet = jdbcTemplate.queryForRowSet(sqlQuery);
+        } else {
+            MapSqlParameterSource parameters = new MapSqlParameterSource("ids", ids);
+            sqlQuery = "SELECT * FROM films_genres fg JOIN genres g ON fg.id_genre = g.id_genre " +
+                    "WHERE fg.id_film IN (:ids)";
+            rowSet = parameterJdbcTemplate.queryForRowSet(sqlQuery, parameters);
+        }
+        Map<Integer, Set<Genre>> genres = new HashMap<>();
+        while (rowSet.next()) {
+            Integer filmId = rowSet.getInt("id_film");
+            Genre genre = Genre.builder()
+                    .id(rowSet.getInt("id_genre"))
+                    .name(rowSet.getString("genre_name"))
+                    .build();
+            if (genres.containsKey(filmId)) {
+                genres.get(filmId).add(genre);
+            } else {
+                genres.put(filmId, new HashSet<>(Set.of(genre)));
+            }
+        }
+        return genres;
     }
 
     private Integer getFilmLikesCount(Integer filmId) {
@@ -140,7 +166,7 @@ public class FilmDbStorage implements FilmStorage {
         return jdbcTemplate.queryForObject(sqlQuery, Integer.class, filmId);
     }
 
-    private Film makeFilm(ResultSet rs) throws SQLException {
+    private Film makeFilm(ResultSet rs, Map<Integer, Set<Genre>> map) throws SQLException {
         int filmId = rs.getInt("id_film");
         return Film.builder()
                 .id(filmId)
@@ -152,7 +178,7 @@ public class FilmDbStorage implements FilmStorage {
                         .id(rs.getInt("id_rating"))
                         .name(rs.getString("rating_name"))
                         .build())
-                .genres(getFilmGenresSet(filmId))
+                .genres(map.getOrDefault(filmId, new HashSet<>()))
                 .likesCount(getFilmLikesCount(filmId))
                 .build();
     }
